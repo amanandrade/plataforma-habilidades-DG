4c19579b0d43999044b6a8034903fdc67da369b4
amanandrade <mandrade.amanda@gmail.com>
2020-02-05 19:09:17 -0300

    refazendo ajustes amanda - front e css

---
+++ versao4-laravel/resources/views/usuarios/home.blade copy.php
@@ -0,0 +1,2 @@
+                            <input type="text" class="form-control" style="width: 300px" placeholder="Pesquisar"
+                            <input type="text" class="form-control" style="width: 300px" placeholder="Pesquisar"
