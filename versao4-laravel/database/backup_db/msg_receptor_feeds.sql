2675d475e42555468148cb533daeaf796b6ae5dd
GitSilni <mundonovo1991@gmail.com>
2020-02-13 16:55:38 -0300

    adicionando base de dados preenchida

---
+++ versao4-laravel/database/backup_db/msg_receptor_feeds.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM plataforma_habilidades.msg_receptor_feeds
+LIMIT 0, 1000
+
+-- Date: 2020-02-13 16:37
+*/
+INSERT INTO `` (`id`,`mensagem`,`usuario_id`,`emissor_id`,`created_at`,`updated_at`) VALUES (1,'esta tudo bom',1,6,'2020-02-09 23:49:15','2020-02-09 23:49:15');
+INSERT INTO `` (`id`,`mensagem`,`usuario_id`,`emissor_id`,`created_at`,`updated_at`) VALUES (2,'silva teste',3,6,'2020-02-09 23:50:10','2020-02-09 23:50:10');
+INSERT INTO `` (`id`,`mensagem`,`usuario_id`,`emissor_id`,`created_at`,`updated_at`) VALUES (3,'esta super light',3,7,'2020-02-09 23:55:48','2020-02-09 23:55:48');
+INSERT INTO `` (`id`,`mensagem`,`usuario_id`,`emissor_id`,`created_at`,`updated_at`) VALUES (4,'isso',1,7,'2020-02-10 00:16:52','2020-02-10 00:16:52');
+INSERT INTO `` (`id`,`mensagem`,`usuario_id`,`emissor_id`,`created_at`,`updated_at`) VALUES (5,'blz',1,6,'2020-02-10 01:17:32','2020-02-10 01:17:32');
---
+++ versao4-laravel/database/backup_db/tags.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: SELECT * FROM plataforma_habilidades.tags
+LIMIT 0, 1000
+
+-- Date: 2020-02-13 16:41
+*/
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (1,1,6,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (2,1,7,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (3,2,9,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (4,2,7,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (5,2,1,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (6,2,2,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (7,2,3,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (8,4,2,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (9,4,5,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (10,4,6,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (11,3,1,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (12,1,1,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (13,1,9,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (14,5,7,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`habilidade_id`,`created_at`,`updated_at`) VALUES (15,5,1,NULL,NULL);
